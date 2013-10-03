a77e6508180716269322bcee496f26d9c57e344a
Mat Ryer <mat@stretchr.com>
2013-10-03 15:36:41 -0600

    added ability to test

--- src/over.js
+++ src/over.js
@@ -1,13 +1,80 @@
 (function(global){
 
   global.MakeOver = function(){
-    return function(){};
+    return function(){
+
+    };
   };
 
+  /**
+   * MakeOver makes a new Over function (useful for testing).
+   */
   global.Over = global.MakeOver();
-  global.Over.signature = function(){
 
+  /**
+   * An object containing functions that can check individual arguments
+   * and make decisions on whether they are indeed something or not.
+   */
+  global.Over.is = {
+    "string": function(v){ return typeof(v)==="string"; },
+    "number": function(v){ return typeof(v)==="number"; },
+    "object": function(v){ return typeof(v)==="object"; },
+    "etc": function(){ return global.Over.etc; }
+  };
 
+  /**
+   * A special reference object that means whatever the arguments are,
+   * they're OK.
+   */
+  global.Over.etc = {};
+
+  /**
+   * Checks arguments against a signature array.
+   */
+  global.Over.test = function(sig, args){
+
+    var result = true;
+    for (var i = 0; i < Math.max(sig.length, args.length); i++) {
+
+      var sigItem = sig[i];
+      var argItem = args[i];
+
+      if (typeof(sigItem)==="undefined") {
+        // no sig item
+        return false;
+      }
+
+      try {
+        result = sigItem(argItem);
+      } catch(e) {
+        return false;
+      }
+
+      if (result === false) {
+        return false;
+      } else if (result === global.Over.etc) {
+        return true;
+      }
+
+    }
+
+    return result;
+  };
+
+  /**
+   * Gets an array of is methods to be called to test a
+   * method call, based on the specified function.
+   */
+  global.Over.signature = function(f){
+
+    var sig = [];
+    var args = global.Over.argnames(f);
+    for (var argI in args) {
+      var arg = args[argI];
+      sig.push(global.Over.is[global.Over.checkFuncFromArg(arg)]);
+    }
+
+    return sig;
 
   };
 
@@ -17,5 +84,12 @@
   global.Over.argnames = function(f){
     return f.toString().split("(")[1].split(")")[0].split(", ");
   };
+
+  /**
+   * Gets the name of the checker func from an argument.r
+   */
+  global.Over.checkFuncFromArg = function(arg){
+    return arg.split("$")[1];
+  }
 
 })(window);
--- test/spec/over.js
+++ test/spec/over.js
@@ -11,3 +11,65 @@ describe("Over.argnames", function(){
   });
 
 });
+
+describe("Over.signature", function(){
+
+  it("should get an array of check functions from argument names", function(){
+
+    var sig = Over.signature(function(name$string, age$number, something$object){})
+    expect(sig[0]).toEqual(Over.is.string);
+    expect(sig[1]).toEqual(Over.is.number);
+    expect(sig[2]).toEqual(Over.is.object);
+
+  });
+
+});
+
+describe("Over.test", function(){
+
+  it("should know when they match", function(){
+
+    var sig = Over.signature(function(name$string, age$number, something$object){})
+
+    var arguments = ["str", 0, {}];
+    expect(Over.test(sig, arguments)).toEqual(true);
+
+    var arguments = ["str", 0];
+    expect(Over.test(sig, arguments)).toEqual(false);
+
+    var arguments = ["str"];
+    expect(Over.test(sig, arguments)).toEqual(false);
+
+    var arguments = [];
+    expect(Over.test(sig, arguments)).toEqual(false);
+
+  });
+
+  it("should cope when there are more arguments than signature items", function(){
+
+    var sig = Over.signature(function(name$string, age$number, something$object){})
+
+    var arguments = ["str", 0, {}, "extra"];
+    expect(Over.test(sig, arguments)).toEqual(false);
+
+  });
+
+  it("should deal with the $etc shortcut", function(){
+
+    var sig = Over.signature(function(name$string, $etc){})
+
+    var arguments = ["str", 0, {}, "extra"];
+    expect(Over.test(sig, arguments)).toEqual(true);
+
+    var arguments = ["str", "extra"];
+    expect(Over.test(sig, arguments)).toEqual(true);
+
+    var arguments = ["str"];
+    expect(Over.test(sig, arguments)).toEqual(true);
+
+    var arguments = [0];
+    expect(Over.test(sig, arguments)).toEqual(false);
+
+  });
+
+});
