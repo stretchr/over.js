35fe5ed428558f9c3ca97b0f57301437822433e3
Mat Ryer <mat@stretchr.com>
2013-10-05 10:15:23 -0600

    added v1 release and minified version

---
+++ min/over-min.js
@@ -0,0 +1,28 @@
+/*
+
+  over.js - BETA
+  Elegant function overloading in JavaScript.
+
+  by Mat Ryer and Ryan Quinn
+  Copyright (c) 2013 Stretchr, Inc.
+
+  Please consider promoting this project if you find it useful.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+  software and associated documentation files (the "Software"), to deal in the Software
+  without restriction, including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+  to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies
+  or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(4(g){g.K=4(){3 4(){5 a=g.2.E.L(B,k);3 4(){9(5 i m a){l(g.2.A(a[i].z,k)){3 a[i].w.L(B,k)}}}}};g.2=g.K();g.2.6=4(t,v){3 h(v)===t};g.2.7={"C":4(v){3 g.2.6("C",v)},"D":4(v){3 g.2.6("D",v)},"r":4(v){3 v!=j&&g.2.6("r",v)&&h(v.p)==="8"},"O":4(v){3 v!=j&&g.2.6("r",v)&&h(v.p)!=="8"},"u":4(v){3 g.2.6("u",v)},"4":4(v){3 g.2.6("4",v)},"j":4(v){3 v===j},"8":4(v){3 g.2.6("8",v)},"M":4(v){3 g.2.7["j"](v)||g.2.7["8"](v)},"n":4(){3 g.2.n}};g.2.7.P=g.2.7["u"];g.2.n={};g.2.E=4(){5 a=[];9(5 i m k){5 b=k[i];a.x({"z":g.2.y(b),"w":b})}3 a};g.2.A=4(a,b){9(5 i=0;i<Y.Z(a.p,b.p);i++){5 c=a[i];5 d=b[i];l(h(c)==="8"){3 q}5 f;Q{f=c(d)}12(e){F.G(e);3 q}l(f===q){3 q}N l(f===g.2.n){3 H}}3 H};g.2.y=4(f){5 a=[];5 b=g.2.I(f);9(5 c m b){5 d=b[c];5 e=g.2.7[g.2.J(d)];l(h(e)==="8"){F.G("R.S: T U 9 \'"+d+"\'.  V W 2.7[\\""+d+"\\"] = 4(v){};")}a.x(e)}3 a};g.2.I=4(f){5 a=f.X().o("(")[1].o(")")[0].o(",");9(5 i m a)a[i]=a[i].10(/^\\s+|\\s+$/g,\'\');3 a};g.2.J=4(a){3 a.o("$")[1]}})(11);',62,65,'||Over|return|function|var|_isType|is|undefined|for||||||||typeof||null|arguments|if|in|etc|split|length|false|object|||boolean||func|push|signature|sig|test|this|string|number|map|console|warn|true|argnames|checkFuncFromArg|MakeOver|apply|nothing|else|array|bool|try|over|js|Unknown|checker|Try|adding|toString|Math|max|replace|window|catch'.split('|'),0,{}));
